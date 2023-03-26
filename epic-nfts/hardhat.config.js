15aa0ce701671632e503cef57979c6dd0c6ba0e4
hsn2222 <hosakanohoka0513@gmail.com>
2023-03-26 20:42:02 +0900

    first commit

---
+++ epic-nfts/hardhat.config.js
@@ -0,0 +1,19 @@
+// npx hardhat run scripts/deploy.js --network goerli
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
